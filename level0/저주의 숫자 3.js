95db822c467862f872dfddea194c9fadef834e60
minsun0714 <woooooooow22@gmail.com>
2022-12-27 10:56:01 +0900

    ✅ for문 풀이 /조건에 부합하는 숫자 배열에서 n-1번째 숫자 반환

---
+++ level0/저주의 숫자 3.js
@@ -0,0 +1,22 @@
+//나의 풀이
+function solution(n) {
+  let counter = 0;
+  for (
+    let i = 0;
+    i < n;
+    counter % 3 === 0 || (counter + "").split("").includes("3") ? i : i++
+  ) {
+    counter++;
+    counter % 3 === 0 ? counter++ : null;
+    (counter + "").split("").includes("3") ? counter++ : null;
+  }
+  return counter;
+}
+
+//다른 풀이
+function solution(n) {
+  return [...Array(n * 2)]
+    .map((_, i) => i + 1)
+    .filter((num) => num % 3 !== 0 && !num.toString().includes("3"))[n - 1];
+}
+console.log(solution(15));
