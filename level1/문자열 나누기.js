98af733111d2686e8101ea4c771d68ed9d620753
minsun0714 <woooooooow22@gmail.com>
2023-01-25 13:49:53 +0900

    ✅ 문자열 나누기

---
+++ level1/문자열 나누기.js
@@ -0,0 +1,29 @@
+// 나의 코드
+function solution(s) {
+  let arr = [];
+  let [firstLetter, lastIndex, same, dif] = [s[0], 0, 0, 0];
+  s.split("").forEach((v, i, a) => {
+    v === firstLetter ? same++ : dif++;
+    if (same === dif) {
+      arr.push(s.substring(lastIndex, i + 1));
+      [firstLetter, lastIndex, same, dif] = [a[i + 1], i + 1, 0, 0];
+    }
+  });
+  return arr.join("").length === s.length ? arr.length : arr.length + 1;
+}
+
+//다른 코드
+function solution(s, count = 0) {
+  console.log();
+  if (!s) return count;
+  let [first, ...rest] = s.split("");
+  let countSame = 1;
+  let countDif = 0;
+  let i = 0;
+  for (; i < rest.length; i++) {
+    if (rest[i] === first) countSame++;
+    else countDif++;
+    if (countSame === countDif) break;
+  }
+  return solution(rest.slice(i + 1).join(""), count + 1);
+}
